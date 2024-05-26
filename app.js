05ea4590074c7d025497ed10a03a286600aec47f
Ori assia <oriassia@gmail.com>
2024-05-26 12:37:02 +0300

    ori v0.3

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 let worldLibraryUrl = "https://www.googleapis.com/books/v1/volumes?q=a&key=AIzaSyAj03UVAQuKO-sABUxmqOPr8gMxJrna9TQ"
 let localLibraryUrl = "http://localhost:8001/books"
 
-const maxBooks = 200; // Maximum number of books to fetch
+const maxBooks = 10; // Maximum number of books to fetch
 
 async function postBooksData() {
     let totalBooks = 0;
-    let startindex = 1
+    let startIndex = 1
     while(totalBooks < maxBooks){
-        const items = await fetchBooks(startindex);
-        startindex += 40 ;
-        for (const item of items) {
+        const items = await fetchBooks(startIndex);
+        for (let item of items) {
             const volumeInfo = item.volumeInfo || {};
-            axios.post(localLibraryUrl,{
+            await axios.post(localLibraryUrl,{
                 name: volumeInfo.title || 'No Title',
                 author: volumeInfo.authors[0] || 'Unknown',
                 num_pages: volumeInfo.pageCount || 0,
@@ -21,16 +20,22 @@ async function postBooksData() {
                 categories: volumeInfo.categories || ['Uncategorized'],
                 ISBN: volumeInfo.industryIdentifiers ? volumeInfo.industryIdentifiers.find(id => id.type === 'ISBN_10' || id.type === 'ISBN_13')?.identifier : 'N/A'
             });
-            totalBooks += 40;
+            totalBooks ++;
             if (totalBooks >= maxBooks) break;
         }
-        if (totalBooks >= maxBooks) break;
+        startIndex += 10; // Move to the next set of books
     }
 }
 
 
-async function fetchBooks(startindex){
-       return await axios.get(`${worldLibraryUrl}&startIndex=${startindex}`)
+async function fetchBooks(startIndex) {
+    try {
+        const response = await axios.get(`${worldLibraryUrl}&startIndex=${startIndex}`);
+        return response.data.items || [];
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        return [];
     }
+}
 
-
+    // postBooksData()
